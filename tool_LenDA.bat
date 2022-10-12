bdbcb1070c8756de621b9e45648ee55f1618eb5f
Danis W <108304436+DanisHome@users.noreply.github.com>
2022-10-12 18:32:19 +0200

    Add files via upload

---
+++ tool_LenDA.bat
@@ -0,0 +1,52 @@
+@echo off
+chcp 65001
+:again
+cls
+color 2
+:main
+echo.
+echo         Welcome too
+echo ██╗     ███████╗███╗   ██╗██████╗  █████╗ 
+echo ██║     ██╔════╝████╗  ██║██╔══██╗██╔══██╗
+echo ██║     █████╗  ██╔██╗ ██║██║  ██║███████║
+echo ██║     ██╔══╝  ██║╚██╗██║██║  ██║██╔══██║
+echo ███████╗███████╗██║ ╚████║██████╔╝██║  ██║
+echo ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ version 2.0
+echo Made by: Danis
+echo ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+echo Lenni i hope you will come back soon!
+echo --------------------------------------------------------------
+echo [1] TempMail(s)                   [8] Spotify
+echo [2] Replit                        [9] Discord
+echo [3] GitHub                        [10] RobloxCheat (Krnl)
+echo [4] Free DesingPlat. (Canva)      [11] CookieClicker               
+echo [5] GTAV ModMenü                  [12] Soon
+echo [6] YouTube                       [13] Soon 
+echo [7] Best GermanServerHoster       [14] Soon
+echo 
+echo [100] Donate
+echo.
+echo -------
+echo Support
+echo -------
+echo https://discord.gg/as9Jsx4A
+echo.
+:mainlogo
+set /p main=Choose your number:
+if %main% == 1 start https://temp-mail.org
+if %main% == 2 start https://replit.com
+if %main% == 3 start https://github.com
+if %main% == 4 start https://canva.com
+if %main% == 5 start https://radicmenu.xyz
+if %main% == 6 start https://youtube.com
+if %main% == 7 start https://prohosting24.de
+if %main% == 8 start https://spotify.com
+if %main% == 9 start https://discord.com
+if %main% == 10 start https://krnl.place 
+if %main% == 11 start https://store.steampowered.com/app/1454400/Cookie_Clicker/
+if %main% == 12 start https://gloobus.tech
+if %main% == 13 start https://gloobus.tech
+if %main% == 14 start https://gloobus.tech
+if %main% == 100 start https://gloobus.tech  
+
+
